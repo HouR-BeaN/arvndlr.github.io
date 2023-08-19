6067505c33b4f011c2484004fd05a05ba4f1d8d2
HouR-BeaN <92734226+HouR-BeaN@users.noreply.github.com>
2023-08-19 23:14:08 +0800

    Revert "Revert "first commit""
    This reverts commit 237382ed36d94afba61bf4ecbd851032e86499b1.

---
+++ src/component/Hairstylist.jsx
@@ -0,0 +1,221 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+
+const Hairstylist = () => {
+  return (
+    <div>
+      <section id="hairstylist" className="p-5 bg-dark">
+        <div className="container">
+          <h2 className="text-center text-white p-5">Our Hairstylist</h2>
+          <p className="lead text-center text-white mb-5">
+            Our hairstylist all have 5+ years working experience in the hair
+            care industry
+          </p>
+          <div className="row g-4">
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-light">
+                <div className="card-body text-center">
+                  <img
+                    src="https://randomuser.me/api/portraits/men/11.jpg"
+                    className="rounded-circle mb-3"
+                    alt=""
+                  />
+                  <h3 className="card-title mb-3">John Doe</h3>
+                  <p className="card-text fw-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Assumenda accusamus nobis sed cupiditate iusto? Quibusdam.
+                  </p>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faTwitter}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faFacebook}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faLinkedin}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faInstagram}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-light">
+                <div className="card-body text-center">
+                  <img
+                    src="https://randomuser.me/api/portraits/women/11.jpg"
+                    className="rounded-circle mb-3"
+                    alt=""
+                  />
+                  <h3 className="card-title mb-3">Jane Doe</h3>
+                  <p className="card-text fw-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Assumenda accusamus nobis sed cupiditate iusto? Quibusdam.
+                  </p>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faTwitter}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faFacebook}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faLinkedin}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faInstagram}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-light">
+                <div className="card-body text-center">
+                  <img
+                    src="https://randomuser.me/api/portraits/men/12.jpg"
+                    className="rounded-circle mb-3"
+                    alt=""
+                  />
+                  <h3 className="card-title mb-3">Steve Smith</h3>
+                  <p className="card-text fw-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Assumenda accusamus nobis sed cupiditate iusto? Quibusdam.
+                  </p>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faTwitter}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faFacebook}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faLinkedin}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faInstagram}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-6 col-lg-3">
+              <div className="card bg-light">
+                <div className="card-body text-center">
+                  <img
+                    src="https://randomuser.me/api/portraits/women/12.jpg"
+                    className="rounded-circle mb-3"
+                    alt=""
+                  />
+                  <h3 className="card-title mb-3">Sara Smith</h3>
+                  <p className="card-text fw-light">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Assumenda accusamus nobis sed cupiditate iusto? Quibusdam.
+                  </p>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faTwitter}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faFacebook}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faLinkedin}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                  <a href="#">
+                    <FontAwesomeIcon
+                      icon={faInstagram}
+                      size="1x"
+                      color="#0077B5"
+                      style={{ margin: "0 3px" }}
+                    />
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Hairstylist;
---
+++ src/component/HomePage.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import About from "./About";
+import Contact from "./Contact";
+import Hairstylist from "./Hairstylist";
+
+const HomePage = () => {
+  return (
+    <div>
+      <section
+        id="home"
+        className="bg-dark text-light p-lg-0 pt-lg text-center text-sm-start"
+      >
+        <div className="container">
+          <div className="d-sm-flex align-items-center justify-content-between p-5">
+            <div>
+              <h1 className="fs-1">
+                Let Us <span className="text-primary">Groom</span> Your
+                <span className="text-primary"> Hair </span>
+              </h1>
+              <p className="lead my-4">
+                We help you to find the best hair-cut for you that suits your
+                identiy and express your fashion.
+              </p>
+              <button
+                className="btn btn-primary btn-lg"
+                data-bs-toggle="modal"
+                data-bs-target="#enroll"
+              >
+                Get Your Haircut
+              </button>
+            </div>
+            <img
+              className="img-fluid w-50 d-none d-sm-block p-5"
+              src="/img/showcase.svg"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
+      <section className="news-letter-cont bg-secondary ext-light py-4 px-5">
+        <div className="d-md-flex justify-content-between align-items-center">
+          <h3 className="mb-3 mb-md-0 text-light">Sign Up For Newsletter</h3>
+        </div>
+        <div className="input-group news-input">
+          <input
+            type="text"
+            className="form-control my-1"
+            placeholder="Enter Email"
+          />
+          <button className="btn btn-dark ms-1" type="button">
+            Submit
+          </button>
+        </div>
+      </section>
+      <About />
+      <Hairstylist />
+      <Contact />
+    </div>
+  );
+};
+
+export default HomePage;
